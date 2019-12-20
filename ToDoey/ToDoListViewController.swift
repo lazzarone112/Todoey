5568d36b1e975c6b9a5d96b564f325090969ecce
M <mohamed.aboghali@gmail.com>
2019-12-20 19:15:31 +0200

    2

--- ToDoey/ToDoListViewController.swift
+++ ToDoey/ToDoListViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["Study","WorkOut","Read"]
+    var itemArray = ["Study","WorkOut","Read"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return itemArray.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
@@ -32,6 +33,33 @@ class ToDoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
     }
+    
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textWaseet = UITextField()
+        
+        let alert = UIAlertController(title: "Add a Task", message: "Add Your Task Name In The Field Under!", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            self.itemArray.append(textWaseet.text!)
+            print(self.itemArray)
+            self.tableView.reloadData()
+            
+        }
+        alert.addAction(action)
+        alert.addTextField { (textField) in
+            textField.placeholder = "Here : )"
+            textWaseet = textField
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
 
 }
 
